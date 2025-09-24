2b1c820fb53cf6576d0339de47567b490bbf440c
Enzo Tomas <195832517+ezTomas@users.noreply.github.com>
2025-09-24 11:04:20 -0300

    Disparo

--- Assets/Script/BalaController.cs
+++ Assets/Script/BalaController.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class BalaController : MonoBehaviour
 {
+    private Rigidbody bullet;
+
+    public float power = 20f;
+    public float timeLife = 2f;
+
+    private float time = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        bullet = GetComponent<Rigidbody>();
+        bullet.linearVelocity = this.transform.forward * power;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        time += Time.deltaTime;
+
+        if (time >= timeLife) 
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Script/WeaponController.cs
+++ Assets/Script/WeaponController.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
+
+    public Transform shootSpawn;
+
+    public bool shooting;
+
+    public GameObject bulletPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +17,21 @@ public class WeaponController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+
+        shooting = Input.GetKeyDown(KeyCode.Mouse0);
+
+        if (shooting)
+        {
+            InstantiateBullet();
+        }
+
     }
+
+    public void InstantiateBullet()
+    {
+        Instantiate(bulletPrefab, shootSpawn.position, shootSpawn.rotation);
+    }
+    
 }
+
